29f8a51b31d8762441de7fdf845d5c3b298d607f
sss <kamikadze.ozersk@gmail.com>
2022-09-17 15:52:01 +0300

    fast fix

--- Main/Models/CommandLineOptions.cs
+++ Main/Models/CommandLineOptions.cs
@@ -1,9 +1,13 @@
 ﻿using CommandLine;
+using CommandLine.Text;
+
+using System.Collections.Generic;
 
 namespace Main.Models
 {
     internal class CommandLineOptions
     {
+        
         [Option(longName:"domain",Required =true,HelpText ="Message broker domain")]
         public string Domain { get; set; }
         [Option(longName:"username",Required =true)]
@@ -12,5 +16,14 @@ namespace Main.Models
         public string Password { get; set; }
         [Option(longName:"timeout",Required = false,HelpText ="get queue timeout",Default =0)]
         public int Timeout { get; set; }
+        [Usage(ApplicationAlias = ".\\RabbitMQSniffer.exe")]
+        public static IEnumerable<Example> Examples 
+        {
+            get
+            {
+                yield return new Example("Default", new CommandLineOptions { Domain = "http://localhost:15672", Username = "guest", Password = "guest" });
+            }
+        }
+       
     }
 }
